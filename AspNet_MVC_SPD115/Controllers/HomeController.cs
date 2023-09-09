505a4294f46712e9cd09ddcd202e7724036d9037
vladtymo <tymo.vlad@gmail.com>
2023-09-09 14:50:28 +0300

    Add project files.

---
+++ AspNet_MVC_SPD115/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AspNet_MVC_SPD115.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNet_MVC_SPD115.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            // code...
+            return this.View(); // ~/Views/Home/Index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNet_MVC_SPD115/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNet_MVC_SPD115.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
