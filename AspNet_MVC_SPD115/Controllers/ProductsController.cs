b811d6d47b70c3efef36a54f01b7fbd7ac4e6b29
vladtymo <tymo.vlad@gmail.com>
2023-09-16 14:27:05 +0300

    Create product list page.

---
+++ AspNet_MVC_SPD115/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using AspNet_MVC_SPD115.Data;
+using AspNet_MVC_SPD115.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_MVC_SPD115.Controllers
+{
+    public class ProductsController : Controller
+    {
+        Shop115DbContext ctx = new Shop115DbContext();
+
+        public IActionResult Index()
+        {
+            // get products from db
+            var products = ctx.Products.ToList();
+            
+            return View(products); // set view mode
+        }
+    }
+}
