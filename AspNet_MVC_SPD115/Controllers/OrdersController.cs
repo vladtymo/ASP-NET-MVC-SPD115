c6b3863eae637305420e9399caac32c9509c7a41
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-10-07 15:47:36 +0300

    Use Authorize attribute.

---
+++ AspNet_MVC_SPD115/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_MVC_SPD115.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
